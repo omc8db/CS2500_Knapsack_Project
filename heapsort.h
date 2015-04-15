080f932c5b8e99a8eb6b4b5f0165f7a86cb205a7
Alex Shamroe <ajsc77@mst.edu>
2015-04-15 11:07:06 -0500

    heapsort template implemented

--- heapsort.h
+++ heapsort.h
@@ -1,7 +1,7 @@
 /*
  * heapsort.h
  *
- * author: Alex Shamroe
+ * author: Owen Chiaventone
  *
  * Driver for comparison of MergeSort and
  * HeapSort
@@ -10,24 +10,26 @@
 
 #ifndef HEAPSORT_H
 #define HEAPSORT_H
+#include <vector>
 
 #include <assert.h>
 
+template <typename T>
 class HeapSort
 {
 public:
 	//This function will do the sorting of the heap array passed to it
 	//Pre:none
 	//Post:The heap array passed to it will be sorted
-  void heapsort(int* array, int size);
+  void heapsort(std::vector<T>& array, int size);
   //This function will turn the array passed to it into a heap
   //Pre:none
   //Post:the array passed to it will be arranged in a heap
-  void buildheap( int* array, int size);
+  void buildheap(std::vector<T>& array, int size);
   //This function will move around nodes in the heap to sort it 
   //Pre:none
   //Post:The heap at that root will be sorted
-  void heapify(int* array, int i, int size);
+  void heapify(std::vector<T>& array, int i, int size);
 
 private:
 };
